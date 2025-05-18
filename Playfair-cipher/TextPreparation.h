cc9d7c3f0114f1ef757e8c3ef390d6708287d46d
Haifisch <127771331+Seek-and-Destroy@users.noreply.github.com>
2025-05-18 23:23:18 +0300

    Delete Playfair-cipher directory

--- Playfair-cipher/TextPreparation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// Класс для подготовки входного текста: очистка, замена J на I, разбиение на биграммы
-class TextPreparation {
-public:
-    // Обрабатывает текст: удаляет не-буквы, приводит к верхнему регистру, добавляет X при необходимости
-    static std::string prepareText(const std::string& text);
-};
--- Playfair-cipher/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Visualizer.h"
-
-// Точка входа программы
-int main() {
-    Visualizer visualizer; // Создаём объект визуализатора
-    visualizer.run(); // Запускаем основной цикл
-    return 0;
-}
